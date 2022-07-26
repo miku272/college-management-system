34c3137353931b211614a2edb321e73273e23800
Naresh Sharma <sharmanaresh272@gmail.com>
2022-07-26 19:46:11 +0530

    .

---
+++ admin/classes.php
@@ -0,0 +1,66 @@
+<?php include('../includes/config.php'); ?>
+<?php include('header.php'); ?>
+<?php include('side_bar.php'); ?>
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Manage Accounts</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Accounts</a></li>
+                    <li class="breadcrumb-item active"><?php echo $_REQUEST['user'] ?></li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <!-- Info boxes -->
+        <div class="table-responsive bg-white">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Sr no.</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $user_query = "SELECT * FROM accounts WHERE user_type='" . $_REQUEST['user'] . "'";
+                    $user_result = mysqli_query($con, $user_query);
+                    $count = 1;
+
+                    while ($users = mysqli_fetch_object($user_result)) {
+                    ?>
+                        <tr>
+                            <td><?php echo $count; ?></td>
+                            <td><?php echo $users->user_name; ?></td>
+                            <td><?php echo $users->email; ?></td>
+                            <td></td>
+                        </tr>
+                    <?php $count++; }  ?>
+                </tbody>
+                <thead>
+                    <tr>
+                        <th>Sr no.</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!--/. container-fluid -->
+</section>
+<!-- /.content -->
+<?php include('footer.php'); ?>
--- admin/side_bar.php
+++ admin/side_bar.php
@@ -228,7 +228,7 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="pages/UI/general.html" class="nav-link">
+                                    <a href="classes.php" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Classes</p>
                                     </a>
@@ -237,6 +237,12 @@
                                     <a href="pages/UI/icons.html" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Section</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="pages/UI/icons.html" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Courses</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
