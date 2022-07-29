b107e0ffd19a519e4cde4660fa6839563b139b7c
Naresh Sharma <sharmanaresh272@gmail.com>
2022-07-29 10:34:16 +0530

    .

---
+++ admin/courses.php
@@ -0,0 +1,134 @@
+<?php include('../includes/config.php'); ?>
+<?php include('header.php'); ?>
+<?php include('side_bar.php'); ?>
+
+<?php
+if (isset($_POST['submit'])) {
+    $title = $_POST['title'];
+    $section_title = implode(',', $_POST['section']);
+    $added_date = date('Y-m-d');
+
+    $query = mysqli_query($con, "INSERT INTO classes (title, section, added_date) VALUES('$title', '$section_title', '$added_date')") or die('oops! Something went wrong...');
+}
+?>
+
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Manage Courses</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                    <li class="breadcrumb-item active">Courses</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <?php if (isset($_REQUEST['action'])) { ?>
+            <div class="card">
+                <div class="card-header py-2">
+                    <h3 class="card-title">
+                        Add New Course
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive bg-white">
+                        <form action="" method="POST">
+                            <div class="form-group px-5 py-3">
+                                <label for="title">Title</label>
+                                <input type="text" name="title" class="form-control bg-white mb-4" placeholder="Enter title" required>
+
+                                <label for="section">Section</label>
+                                <?php
+                                $query = mysqli_query($con, "SELECT * FROM sections");
+                                $count = 1;
+                                while ($sections = mysqli_fetch_object($query)) {
+                                ?>
+                                    <div>
+                                        <label for="<?php $count ?>">
+                                            <input type="checkbox" id="<?php $count ?>" value="<?php echo $sections->title; ?>" name="section[]" class="mb-1" required><?php echo $sections->title; ?>
+                                        </label>
+                                    </div>
+                                <?php $count++;
+                                } ?>
+                            </div>
+                            <input type="submit" name="submit" class="btn btn-success mx-5 mb-2 col-3" value="Add Course">
+                        </form>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+        <?php } else { ?>
+            <!-- Info boxes -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        Courses
+                    </h3>
+                    <div class="card-tools">
+                        <a href="?action=add-new" class="btn btn-sm btn-outline-success"><i class="fa fa-plus mr-2"></i>Add New</a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive bg-white">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Sr no.</th>
+                                    <th></th>
+                                    <th>Name</th>
+                                    <th>Category</th>
+                                    <th>Duration</th>
+                                    <th>Price</th>
+                                    <th>Added Date</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <tbody>
+                                <?php
+                                $query = mysqli_query($con, "SELECT * FROM classes");
+                                $count = 1;
+
+                                while ($class = mysqli_fetch_object($query)) {
+                                ?>
+                                    <tr>
+                                        <td><?php echo $count; ?></td>
+                                        <td><?php echo $class->title; ?></td>
+                                        <td><?php echo $class->section; ?></td>
+                                        <td></td>
+                                    </tr>
+                                <?php $count++;
+                                } ?>
+                            </tbody>
+                            </tbody>
+                            <thead>
+                                <tr>
+                                    <th>Sr no.</th>
+                                    <th></th>
+                                    <th>Name</th>
+                                    <th>Category</th>
+                                    <th>Duration</th>
+                                    <th>Price</th>
+                                    <th>Added Date</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+        <?php } ?>
+        <!--/. container-fluid -->
+</section>
+<!-- /.content -->
+<?php include('footer.php'); ?>
--- admin/footer.php
+++ admin/footer.php
@@ -1,7 +1,7 @@
 </div>
 <!-- Main Footer -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="#">College Management System</a>.</strong>
+    <strong>Copyright &copy; 2014-2021 <a href="https://vtcbcsr.edu.in/">College Management System</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0.0
@@ -19,20 +19,6 @@
 <script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
 <script src="../dist/js/adminlte.js"></script>
-
-<!-- PAGE PLUGINS -->
-<!-- jQuery Mapael -->
-<script src="../plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-<script src="../plugins/raphael/raphael.min.js"></script>
-<script src="../plugins/jquery-mapael/jquery.mapael.min.js"></script>
-<script src="../plugins/jquery-mapael/maps/usa_states.min.js"></script>
-<!-- ChartJS -->
-<script src="../plugins/chart.js/Chart.min.js"></script>
-
-<!-- AdminLTE for demo purposes -->
-<script src="../dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="../dist/js/pages/dashboard2.js"></script>
 
 <script>
     (function() {
