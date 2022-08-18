bbeedbc6bac47cc13bca3f309d75ab88490afbd5
Naresh Sharma <sharmanaresh272@gmail.com>
2022-08-18 11:17:19 +0530

    .

--- student/time_table.php
+++ student/time_table.php
@@ -56,7 +56,7 @@
                                 $days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
 
                                 foreach ($days as $day) {
-                                    $query = mysqli_query($con, "SELECT * FROM posts AS p INNER JOIN metadata AS mc ON (mc.item_id = p.id) INNER JOIN metadata AS md ON (md.item_id = p.id) INNER JOIN metadata AS mp ON (mp.item_id = p.id) WHERE p.type = 'time_table' AND p.status = 'publish' AND md.meta_key = 'day_name' AND md.meta_value = '$day' AND mp.meta_key = 'period_id' AND mp.meta_value = $period->id AND mc.meta_key = 'class_id AND mc.meta_value = 1");
+                                    $query = mysqli_query($con, "SELECT * FROM posts as p INNER JOIN metadata as mc ON (mc.item_id = p.id) INNER JOIN metadata as md ON (md.item_id = p.id) INNER JOIN metadata as mp ON (mp.item_id = p.id) WHERE p.type = 'timetable' AND p.status = 'publish' AND md.meta_key = 'day_name' AND md.meta_value = '$day' AND mp.meta_key = 'period_id' AND mp.meta_value = $period->id AND mc.meta_key = 'class_id' AND mc.meta_value = 1");
 
                                     if (mysqli_num_rows($query) > 0) {
                                         while ($time_table = mysqli_fetch_object($query)) {
